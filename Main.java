401de63cb44ffa9fad50af456eb86bae0cc86fe2
Karthikeya <viratkarthikeya647@gmail.com>
2026-03-23 00:27:50 +0530

    oo

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-public class main
-public static void main(string[]args){
-    system.out.println(x:"hello from jenkins!");
+public class Main{
+public static void main(String[] args){
+    System.out.println(x:"hello from jenkins!");
+}
 }
